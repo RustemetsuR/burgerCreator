c6890423cebf6ef67c64090302b87fe7f5d525c5
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-07-29 20:03:14 +0600

    Added divs for burger

--- burger-creator/src/containers/App.js
+++ burger-creator/src/containers/App.js
@@ -1,23 +1,29 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
   const INGREDIENTS = [
-    {name: 'Meat', price: 50},
-    {name: 'Salad', price: 5},
-    {name: 'Cheese', price: 20},
-    {name: 'Beacon', price: 30},
+    { name: 'Meat', price: 50 },
+    { name: 'Salad', price: 5 },
+    { name: 'Cheese', price: 20 },
+    { name: 'Beacon', price: 30 },
   ];
 
   const [ingredients, setIngredients] = useState([
-    {name: 'Meat', count: 0},
-    {name: 'Salad', count: 0},
-    {name: 'Cheese', count: 0},
-    {name: 'Beacon', count: 0},
+    { name: 'Meat', count: 0 },
+    { name: 'Salad', count: 0 },
+    { name: 'Cheese', count: 0 },
+    { name: 'Beacon', count: 0 },
   ]);
   return (
     <div className="App">
-      
+      <div className="Burger">
+        <div class="BreadTop">
+          <div class="Seeds1"></div>
+          <div class="Seeds2"></div>
+        </div>
+        <div class="BreadBottom"></div>
+      </div>
     </div>
   );
 }
