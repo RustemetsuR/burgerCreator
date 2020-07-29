b198c95fbb9e3702b6887fc479be3a57cf0a3939
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-07-29 19:34:25 +0600

    Added INGREDIENTS array with all ingredients and prices

--- burger-creator/src/App.js
+++ burger-creator/src/App.js
@@ -2,23 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const INGREDIENTS = [
+    {name: 'Meat', price: 50},
+    {name: 'Salad', price: 5},
+    {name: 'Cheese', price: 20},
+    {name: 'Beacon', price: 30},
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
