c502fadc34bad264a247bfb913e44015d309757d
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-07-30 16:19:00 +0600

    Added addIngredient() method to add ingredient to the burger, increase the total price and increase amount of ingridient. Added deleteOnlyOneIngridient() method to delete one oneIngridient, reduce the total price and reduce amount of the ingredient. Added deleteAllIngredient() Method to remove all ingredients of one kind, reduce the total price,and set amount of ingredients to 0. Added AddIngredientList, BurgerVisualIngredient and TotalPrice components

--- burger-creator/src/containers/App.js
+++ burger-creator/src/containers/App.js
@@ -1,29 +1,108 @@
 import React, { useState } from 'react';
 import './App.css';
+import TotalPrice from '../components/TotalPrice/TotalPrice';
+import BurgerVisualIngredient from '../components/BurgerVisualIngredient/BurgerVisualIngredient'
+import AddIngredientList from '../components/AddIngredientList/AddIngredientList';
 
 const App = () => {
-  const INGREDIENTS = [
-    { name: 'Meat', price: 50 },
-    { name: 'Salad', price: 5 },
-    { name: 'Cheese', price: 20 },
-    { name: 'Beacon', price: 30 },
-  ];
 
   const [ingredients, setIngredients] = useState([
-    { name: 'Meat', count: 0 },
-    { name: 'Salad', count: 0 },
-    { name: 'Cheese', count: 0 },
-    { name: 'Beacon', count: 0 },
+    { name: 'Meat', count: 0, price: 50, index: 0 },
+    { name: 'Salad', count: 0, price: 5, index: 1 },
+    { name: 'Cheese', count: 0, price: 20, index: 2 },
+    { name: 'Bacon', count: 0, price: 30, index: 3 },
   ]);
+
+  const [totalPrice, setTotalPrice] = useState(20);
+
+  const [burgerVisualIngredients, setBurgerVisualIngredients] = useState([]);
+
+  const [burgerVisualIngredientsIndex, setBurgerVisualIngredientsIndex] = useState(0);
+
+  const addIngredient = index => {
+    let ingredientsCopy = [...ingredients];
+    let totalPriceCopy = totalPrice;
+    let burgerVisualIngredientsCopy = [...burgerVisualIngredients];
+    let burgerVisualIngredientsIndexCopy = burgerVisualIngredientsIndex;
+    if (ingredientsCopy[index].count !== 5) {
+      burgerVisualIngredientsCopy.push({
+        name: ingredientsCopy[index].name,
+        index: burgerVisualIngredientsIndexCopy,
+      })
+      ingredientsCopy[index].count++;
+      totalPriceCopy = ingredientsCopy[index].price + totalPriceCopy;
+      burgerVisualIngredientsIndexCopy++;
+    }
+    setTotalPrice(totalPriceCopy);
+    setIngredients(ingredientsCopy);
+    setBurgerVisualIngredients(burgerVisualIngredientsCopy);
+    setBurgerVisualIngredientsIndex(burgerVisualIngredientsIndexCopy);
+  }
+
+  const deleteOnlyOneIngredient = index => {
+    let ingredientsCopy = [...ingredients];
+    let totalPriceCopy = totalPrice;
+    let burgerVisualIngredientsCopy = [...burgerVisualIngredients];
+    if (ingredientsCopy[index].count !== 0) {
+      ingredientsCopy[index].count--;
+      totalPriceCopy = totalPriceCopy - ingredientsCopy[index].price;
+      for (let i = 0; i < burgerVisualIngredientsCopy.length; i++) {
+        if (burgerVisualIngredientsCopy[i].name === ingredientsCopy[index].name) {
+          burgerVisualIngredientsCopy.splice(i, 1);
+          break;
+        }
+      }
+    }
+    setTotalPrice(totalPriceCopy);
+    setIngredients(ingredientsCopy);
+    setBurgerVisualIngredients(burgerVisualIngredientsCopy);
+  }
+
+  const deleteAllIngredients = index => {
+    let ingredientsCopy = [...ingredients];
+    let totalPriceCopy = totalPrice;
+    let burgerVisualIngredientsCopy = [...burgerVisualIngredients];
+    if (ingredientsCopy[index].count !== 0) {
+      totalPriceCopy = totalPriceCopy - ingredientsCopy[index].price * ingredientsCopy[index].count;
+      ingredientsCopy[index].count = 0;
+      for (let i = 0; i < burgerVisualIngredientsCopy.length; i++) {
+        if (burgerVisualIngredientsCopy[i].name === ingredientsCopy[index].name) {
+          burgerVisualIngredientsCopy.splice(i, 1);
+          i--;
+        }
+      }
+    }
+    setBurgerVisualIngredients(burgerVisualIngredientsCopy);
+    setTotalPrice(totalPriceCopy);
+    setIngredients(ingredientsCopy);
+  }
+
   return (
     <div className="App">
-      <div className="Burger">
-        <div class="BreadTop">
-          <div class="Seeds1"></div>
-          <div class="Seeds2"></div>
+      <div className='container'>
+        <div className="create-burger-box boxes">
+          <h2>Ingredients</h2>
+          <AddIngredientList
+            ingredients={ingredients}
+            deleteAllIngredients={deleteAllIngredients}
+            deleteOnlyOneIngredient={deleteOnlyOneIngredient}
+            addIngredient={addIngredient} />
         </div>
-        <div class="BreadBottom"></div>
+        <div className="burger-box boxes">
+        <h2>Burger</h2>
+          <div className="Burger">
+            <div className="BreadTop">
+              <div className="Seeds1"></div>
+              <div className="Seeds2"></div>
+            </div>
+            <BurgerVisualIngredient burgerVisualIngredients={burgerVisualIngredients} />
+            <div className="BreadBottom"></div>
+          </div>
+          <TotalPrice price={totalPrice} />
+        </div>
+
       </div>
+
     </div>
   );
 }
